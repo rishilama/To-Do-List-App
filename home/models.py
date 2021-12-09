3958836731b7dd8073ec24aba9db484f8d73cdae
RISHI <rishilama03@gmail.com>
2021-12-09 12:49:25 +0530

    user added

--- home/models.py
+++ home/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Tasks(models.Model):
-    
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField(max_length=255)
     description = models.TextField()
     added_date = models.DateTimeField(auto_now_add=True)
